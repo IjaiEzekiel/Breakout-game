94c9a3385ac2f17717849627e0f3efcc513b48cc
Ijai <ijaiwaba@gmail.com>
2022-12-25 16:00:49 +0100

    An improved game

---
+++ index.js
@@ -0,0 +1,235 @@
+const startPauseButton = document.querySelector('#start-pause-button')
+const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('.score')
+const blockWidth = 100
+const blockHeight = 20
+const ballDiameter = 20
+const boardWidth = 560
+const boardHeight = 300
+let timerId
+let xDirection = -2
+let yDirection = 2
+let score = 0
+
+const userStart = [230, 10] 
+let currentPosition = userStart
+
+const ballStart = [270, 40]
+let ballCurrentPosition = ballStart
+
+// create Block
+class Block {
+ constructor (xAxis, yAxis) {
+  this.bottomLeft = [xAxis,yAxis]
+  this.bottomRight = [xAxis + blockWidth, yAxis]
+  this.topLeft = [xAxis, yAxis + blockHeight]
+  this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+ }
+}
+
+// all my blocks
+const blocks = [
+ new Block (10,270),
+ new Block (120,270),
+ new Block (230,270),
+ new Block (340,270),
+ new Block (450,270),
+ new Block (10,240),
+ new Block (120,240),
+ new Block (230,240),
+ new Block (340,240),
+ new Block (450,240),
+ new Block (10,210),
+ new Block (120,210),
+ new Block (230,210),
+ new Block (340,210),
+ new Block (450,210),
+]
+console.log(blocks)
+//draw all my blocks
+function addBlock() {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = blocks[i].bottomLeft[0] + 'px'
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+    grid.appendChild(block)
+  }
+}
+
+addBlock()
+
+//add user
+const user = document.createElement('div')
+user.classList.add('user')
+drawUser()
+grid.appendChild(user)
+
+//draw the user
+function drawUser () {
+ user.style.left = currentPosition[0] + 'px'
+ user.style.bottom = currentPosition[1] + 'px'
+
+}
+
+//draw the ball
+function drawBall() {
+ ball.style.left = ballCurrentPosition[0] + 'px'
+ ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
+
+//move user
+function moveUser(e) {
+ switch(e.key) {
+  case 'ArrowLeft':
+   if (currentPosition[0] > 0) {
+    currentPosition[0] -=10
+    drawUser()
+   }
+   break;
+  
+  case 'ArrowRight':
+   if (currentPosition[0] < boardWidth - blockWidth) {
+    currentPosition[0] += 10
+    drawUser()
+   }
+   break;
+ }
+}
+
+document.addEventListener('keydown', moveUser)
+
+// add ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+grid.appendChild(ball)
+
+//move ball
+function moveBall () {
+ ballCurrentPosition[0] += xDirection
+ ballCurrentPosition[1] += yDirection
+ drawBall()
+ checkForCollisions()
+}
+
+timerId = setInterval(moveBall, 30);
+
+//check for collisions
+function checkForCollisions () {
+  //check for block collisions
+  for (let i = 0; i < blocks.length; i++) {
+    if (
+      (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) && ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1])
+    ) {
+      const allBlocks = Array.from(document.querySelectorAll('.block'))
+      allBlocks[i].classList.remove('block')
+      blocks.splice(i, 1)
+      changeDirection()
+      score++
+      scoreDisplay.innerHTML = score
+
+    //check for win
+    if (blocks.length === 0) {
+      scoreDisplay.classList.remove('score')
+      scoreDisplay.innerHTML = 'You Win'
+      clearInterval(timerId)
+      scoreDisplay.classList.add('won')
+      document.removeEventListener('keydown', moveUser)
+      startPauseButton.innerHTML = "Restart"
+      startPauseButton.addEventListener('click', () => {
+      location.reload()
+ })
+      confettiFy()
+    }
+  }
+
+
+}
+
+  //confetti
+const jsConfetti = new JSConfetti('body')
+function confettiFy() {
+  setTimeout(() => {
+    jsConfetti.addConfetti({
+  confettiColors: [
+    '#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7',
+  ],
+  emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+  confettiNumber: Infinity,
+  confettiRadius: .1,
+  confettiNumber: 500,
+  })
+  }, 100)
+}
+
+
+
+ //check for wall collisions
+ if (
+    ballCurrentPosition[0] >= (boardWidth - ballDiameter) || ballCurrentPosition[1] >= (boardHeight -   ballDiameter) || ballCurrentPosition[0] <= 0
+  ) {
+  changeDirection()
+ }
+
+ //check for user collisions
+ if (
+  (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) && (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight)
+ ) {
+    changeDirection()
+  
+  }
+
+ //check for game over
+ if (ballCurrentPosition[1] <= 0) {
+  clearInterval(timerId)
+  scoreDisplay.innerHTML = 'You Lose'
+  document.removeEventListener('keydown', moveUser)
+  startPauseButton.innerHTML = "Restart"
+  startPauseButton.addEventListener('click', () => {
+    location.reload()
+ })
+ }
+ startPause()
+
+
+  if (ballCurrentPosition[1] <= 0 && !timerId) {
+}
+
+}
+
+function changeDirection () {
+ if (xDirection === 2 && yDirection === 2) {
+  yDirection = -2
+  return 
+ }
+ if (xDirection === 2 && yDirection === -2) {
+  xDirection = -2
+  return
+ }
+ if (xDirection === -2 && yDirection === -2) {
+  yDirection = 2
+  return
+ }
+ if (xDirection === -2 && yDirection === 2) {
+  xDirection = 2
+  return
+ }
+}
+
+//start and pause 
+function startPause() {
+startPauseButton.addEventListener('click', () => {
+  if (timerId) {
+    clearInterval(timerId)
+    document.removeEventListener('keydown', moveUser)
+    startPauseButton.innerHTML = "Start"
+    timerId = null
+  } else {
+    timerId = setInterval(moveBall, 30)
+    document.addEventListener('keyup', moveUser)
+    startPauseButton.innerHTML = "Pause"
+  }
+})
+}
+
+
